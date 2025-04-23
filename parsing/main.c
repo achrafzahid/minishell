7b5c199e13f3c65262c8e394f0d4cc062c3f6cd8
Amine Abbadi <amabbadi@e1r3p10.1337.ma>
2025-04-23 01:25:09 +0100

    added a secret sauce that no one should know that fixes overwriting the current line when using ANSI CODES

--- parsing/main.c
+++ parsing/main.c
@@ -40,7 +40,7 @@ char	*get_prompt(void)
 		else
 			last = cwd;
 		snprintf(prompt, sizeof(prompt),
-			"\x1b[1;36m[minishin]: \x1b[35m~%s\x1b[0m ", last);
+			"\001\x1b[1;36m\002[minishin]: \001\x1b[35m~%s\x1b[0m\002 ", last);
 	}
 	else
 		snprintf(prompt, sizeof(prompt), "\x1b[1;32m[minihell]:~ \x1b[0m");
