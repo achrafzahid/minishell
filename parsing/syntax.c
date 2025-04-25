f96831c82f5b1c035b26cc890bd95d6d207f5759
Amine Abbadi <amabbadi@e2r9p13.1337.ma>
2025-04-25 18:36:47 +0100

    more fixes especially curly braces and other misc stuff

--- parsing/syntax.c
+++ parsing/syntax.c
@@ -4,6 +4,7 @@ int is_syntax_error(char *input)
 {
 	int in_single = 0;
 	int in_double = 0;
+	int opened_brace = 0;
 
 	while (*input == ' ') input++;
 	if (*input == '|')
@@ -15,34 +16,44 @@ int is_syntax_error(char *input)
 	{
 		if (*input == '\'' && !in_double)
 			in_single = !in_single;
-		else if (*input == '\"' && !in_single)
+		else if (*input == '"' && !in_single)
 			in_double = !in_double;
 
 		if (!in_single && !in_double)
 		{
-            if (*input == '|')
-            {
-                input++;
-                while (*input == ' ') input++;
-                if (*input == '|' || *input == '\0' || *input == '<')
-                {
-                    fprintf(stderr, "minishell: syntax error near unexpected token `|'\n");
-                    return 1;
-                }
-            }
+			if (*input == '{')
+				opened_brace = 1;
+			if (*input == '}')
+			{
+				fprintf(stderr, "minishell: syntax error near unexpected token `}'\n");
+				return 1;
+			}
+			if (*input == '|')
+			{
+				input++;
+				while (*input == ' ') input++;
+				if (*input == '|' || *input == '\0' || *input == '<')
+				{
+					fprintf(stderr, "minishell: syntax error near unexpected token `|'\n");
+					return 1;
+				}
+				continue;
+			}
 			if (*input == '>' || *input == '<')
 			{
 				char op = *input;
 				char token[5] = {0};
 				int count = 0;
-
 				while (*input == op && count < 4)
 					token[count++] = *input++;
-
 				token[count] = 0;
 				while (*input == ' ') input++;
-
-				if (*input == '\0' || *input == '|' || *input == '>' || *input == '<')
+				if (*input == '\0')
+				{
+					fprintf(stderr, "minishell: syntax error near unexpected token `newline'\n");
+					return 1;
+				}
+				if (*input == '|' || *input == '>' || *input == '<')
 				{
 					fprintf(stderr, "minishell: syntax error near unexpected token `%s'\n", token);
 					return 1;
@@ -51,6 +62,11 @@ int is_syntax_error(char *input)
 			}
 		}
 		input++;
+	}
+	if (opened_brace)
+	{
+		fprintf(stderr, "minishell: syntax error: unexpected end of file\n");
+		return 1;
 	}
 	return 0;
 }
